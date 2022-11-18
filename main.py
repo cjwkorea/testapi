64443590e2a2fd8c40fb57285a5fa239945082de
cjwkorea <cjwkorea0@gmail.com>
2022-11-18 14:28:46 +0900

    fast-api

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+
+class ResponseDTO(BaseModel):
+    code: int
+    message: str
+    data: object | None
+
 
 class Cat(BaseModel):
     name: str
     id: int = 0
+
 
 app = FastAPI()
 
@@ -16,9 +26,20 @@ async def root(id: int):
 @app.get("/second")
 async def second(skip: int = 0, limit: int = 10):
     # (skip: int = 0, limit: int = 10) 값을 보내지않아도 나오는 기본값
-    return {"skip" : skip,  "limit" : limit}
+    return {"skip": skip, "limit": limit}
 
 
 @app.post("/cat")
 async def cat(cat: Cat):
     return cat
+
+
+@app.get("/error")
+async def error():
+    dto = ResponseDTO(code=0, message="페이지가 없습니다.", data=None)
+    return JSONResponse(status_code=404, content=jsonable_encoder(dto))
+
+
+@app.get("/error1")
+async def error1():
+    raise HTTPException(status_code=404, detail={"message": "item not found"})
