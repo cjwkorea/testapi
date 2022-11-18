771b07b990dbde5cebe4fba6674228fbbcaaf1ef
cjwkorea <cjwkorea0@gmail.com>
2022-11-18 11:29:40 +0900

    fast-api

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Cat(BaseModel):
+    name: str
+    id: int = 0
 
 app = FastAPI()
 
 
-@app.get("/first")
-async def root():
-    return "안녕"
+@app.get("/first/{id}")
+async def root(id: int):
+    return {"message": "Hello World", "id": id}
+
+
+@app.get("/second")
+async def second(skip: int = 0, limit: int = 10):
+    # (skip: int = 0, limit: int = 10) 값을 보내지않아도 나오는 기본값
+    return {"skip" : skip,  "limit" : limit}
+
+
+@app.post("/cat")
+async def cat(cat: Cat):
+    return cat
